7a42e66efedfc91299f1366b59a9dd388766b930
Femifranciskmr <femifranciskmr@gmail.com>
2024-01-07 22:29:11 +0530

    backend for doctor project

---
+++ index.js
@@ -0,0 +1,23 @@
+// import json server
+const jsonServer=require('json-server')
+
+//create a server application using jsonSever
+const restServer=jsonServer.create()
+
+//setup path for db.json
+const router=jsonServer.router()
+
+//return a midlleware used by json server
+const middleware=jsonServer.defaults()
+
+//setup port
+const port=3001
+
+//use in server
+restServer.use(middleware)
+restServer.use(router)
+
+//to run a port
+restServer.listen(port,()=>{
+    console.log('rest sever listening on port',+port);
+})
